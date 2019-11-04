0632503c967c3002f33484c3b8d9f2dad996d3a9
everett-lee <everett.lee@btinternet.com>
2019-11-04 15:19:43 +0000

    add basic login form

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,24 +1,38 @@
 import React from 'react'
-import {Button, Segment, Form } from 'semantic-ui-react'
+import { Button, Divider, Form, Grid, Segment } from 'semantic-ui-react'
 
 function LoginForm() {
-  return (
-    <Segment>
-        <Form>
-            <Form.Field>
-                <label>Username</label>
-                <input placeholder='Email/username' />
-            </Form.Field>
-            <Form.Field>
-                <label>Password</label>
-                <input placeholder='Password' />
-            </Form.Field>
-            <Form.Field>
-                <Button>Submit</Button>
-                <Button>Register</Button>
-            </Form.Field>
-        </Form>
-    </Segment>
+    return (
+        <div className="loginDiv">
+            <Segment placeholder className="loginSegment">
+                <Grid columns={2} relaxed='very' stackable>
+                    <Grid.Column>
+                        <Form>
+                            <Form.Input
+                                icon='user'
+                                iconPosition='left'
+                                label='Username'
+                                placeholder='Username'
+                            />
+                            <Form.Input
+                                icon='lock'
+                                iconPosition='left'
+                                label='Password'
+                                type='password'
+                            />
+
+                            <Button content='Login' primary />
+                        </Form>
+                    </Grid.Column>
+
+                    <Grid.Column verticalAlign='middle'>
+                        <Button content='Sign up' icon='signup' size='big' />
+                    </Grid.Column>
+                </Grid>
+
+                <Divider vertical>Or</Divider>
+            </Segment>
+        </div>
   );
 }
 
