71a2b9c26794b6ffe3ca3c6a1e5e2fddfdeaa29f
Lee <lee.m.everett@gmail.com>
2019-12-05 22:18:04 +0000

    start adding apollo client

--- src/index.js
+++ src/index.js
@@ -2,7 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import { AuthenticationProvider } from './components/providers/AuthenticationProvider';
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
 import 'semantic-ui-css/semantic.min.css';
+
+const cache = new InMemoryCache();
+const link = new HttpLink({
+  uri: 'http://localhost:8080/'
+});
+
+const client = new ApolloClient({
+  cache,
+  link
+});
 
 ReactDOM.render(
     <AuthenticationProvider>
